fccc0d84e54d91a070fddd1bf2e333b728dcfff3
oneseel <seno10065@gmail.com>
2024-01-17 00:30:35 +0900

    feat : 게시글 단건 조회 API 테스트

--- src/test/java/com/gamecommunity/domain/post/service/PostServiceTest.java
+++ src/test/java/com/gamecommunity/domain/post/service/PostServiceTest.java
@@ -1,5 +1,7 @@
 package com.gamecommunity.domain.post.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -7,19 +9,24 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.gamecommunity.domain.post.dto.PostRequestDto;
+import com.gamecommunity.domain.post.dto.PostResponseDto;
 import com.gamecommunity.domain.post.entity.Post;
 import com.gamecommunity.domain.post.repository.PostRepository;
 import com.gamecommunity.domain.test.PostTest;
 import com.gamecommunity.domain.user.entity.User;
 import com.gamecommunity.global.config.SecurityConfig.AuthenticationHelper;
+import com.gamecommunity.global.exception.common.BusinessException;
+import com.gamecommunity.global.exception.common.ErrorCode;
 import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
 import java.io.IOException;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -58,6 +65,43 @@ import org.springframework.web.multipart.MultipartFile;
       // Then
       verify(postImageUploadService, times(1)).uploadFile(file);
       verify(postRepository, times(1)).save(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("게시글 조회 - 성공")
+    void getPostTestSuccess() {
+
+      // given
+      Long postId = TEST_POST_ID;
+      Post post = TEST_POST;
+      given(postRepository.findById(postId)).willReturn(Optional.of(post));
+
+      // when
+      PostResponseDto result = postService.getPost(postId);
+
+      // then
+      assertEquals(post.getPostTitle(), result.postTitle());
+      assertEquals(post.getPostContent(), result.postContent());
+    }
+
+    @Test
+    @DisplayName("게시글 조회 - 실패(게시글을 찾을 수 없음)")
+    void getPostTestFailureNotFoundPost() {
+
+      // given
+      Long postId = TEST_ANOTHER_POST_ID;
+      given(postRepository.findById(postId)).willReturn(Optional.empty());
+
+      // when, then
+      // 예외가 발생하는지 확인하는 로직
+      BusinessException ex = assertThrows(BusinessException.class, () -> {
+        postService.getPost(postId);
+      });
+
+      assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+      assertEquals(ErrorCode.NOT_FOUND_POST_EXCEPTION.getMessage(), ex.getMessage());
+
+      verify(postRepository, times(1)).findById(postId);
     }
 
   }
