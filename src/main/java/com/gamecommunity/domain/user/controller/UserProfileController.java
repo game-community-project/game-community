7c8996e5bb4b6a5787ec8df4574fdbda69e7e03b
Dahee Son <sxxdxh2@gmail.com>
2024-01-17 12:36:12 +0900

    feat : 유저 프로필 수정 API 구현

--- src/main/java/com/gamecommunity/domain/user/controller/UserProfileController.java
+++ src/main/java/com/gamecommunity/domain/user/controller/UserProfileController.java
@@ -1,11 +1,17 @@
 package com.gamecommunity.domain.user.controller;
 
+import com.gamecommunity.domain.user.dto.ModifyProfileDto;
 import com.gamecommunity.domain.user.service.UserProfileService;
 import com.gamecommunity.global.response.ApiResponse;
+import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +29,16 @@ public class UserProfileController {
     return ResponseEntity.ok(ApiResponse.ok("프로필 조회 성공", userProfileService.getProfile(userId)));
   }
 
+  @PatchMapping("/{userId}")
+  public ResponseEntity<ApiResponse> modifyProfile(
+          @PathVariable Long userId,
+          @RequestBody @Valid ModifyProfileDto modifyProfileDto,
+          @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    userProfileService.modifyProfile(userId, modifyProfileDto.introduction(),
+            modifyProfileDto.profile_url(), userDetails);
+
+    return ResponseEntity.ok(ApiResponse.ok("프로필 수정 성공", null));
+  }
 
 }
--- src/main/java/com/gamecommunity/domain/user/service/UserProfileService.java
+++ src/main/java/com/gamecommunity/domain/user/service/UserProfileService.java
@@ -5,6 +5,7 @@ import com.gamecommunity.domain.user.entity.User;
 import com.gamecommunity.domain.user.repository.UserRepository;
 import com.gamecommunity.global.exception.common.BusinessException;
 import com.gamecommunity.global.exception.common.ErrorCode;
+import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserProfileService {
+
   private final UserRepository userRepository;
 
   @Transactional
@@ -21,6 +23,22 @@ public class UserProfileService {
             new BusinessException(HttpStatus.NOT_FOUND, ErrorCode.NOT_FOUND_USER_EXCEPTION));
 
     return UserProfileDto.from(user);
+  }
+
+  @Transactional
+  public void modifyProfile(
+          Long userId,
+          String introduction,
+          String profile_url,
+          UserDetailsImpl userDetails) {
+
+    User user = userRepository.findById(userId).orElseThrow(() ->
+            new BusinessException(HttpStatus.NOT_FOUND, ErrorCode.NOT_FOUND_USER_EXCEPTION));
+    if (!userId.equals(userDetails.getId())) {
+      throw new BusinessException(HttpStatus.UNAUTHORIZED,
+              ErrorCode.AUTHENTICATION_MISMATCH_EXCEPTION);
+    }
+    user.modifyProfile(introduction, profile_url);
   }
 
 }
