1fe907a12e5999bed31941a918e71ac5a23ca61e
minseong1 <alstjd2627@naver.com>
2024-01-18 17:29:31 +0900

    feat : createComment 구현, findByPostId 추가

--- src/main/java/com/gamecommunity/domain/comment/controller/CommentController.java
+++ src/main/java/com/gamecommunity/domain/comment/controller/CommentController.java
@@ -1,5 +1,38 @@
 package com.gamecommunity.domain.comment.controller;
 
+
+import com.gamecommunity.domain.comment.dto.CommentRequestDto;
+import com.gamecommunity.domain.comment.dto.CommentResponseDto;
+import com.gamecommunity.domain.comment.service.CommentService;
+import com.gamecommunity.domain.user.entity.User;
+import com.gamecommunity.global.response.ApiResponse;
+import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/posts/{postId}/comments")
 public class CommentController {
 
+  private final CommentService commentService;
+
+  @PostMapping
+  public ResponseEntity<ApiResponse<CommentResponseDto>> createComment(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long postId,
+      @RequestBody CommentRequestDto commentRequestDto
+  ) {
+    User user = userDetails.getUser();
+    CommentResponseDto commentResponseDto = commentService.createComment(user, postId,
+        commentRequestDto);
+    return ResponseEntity.ok(ApiResponse.ok("댓글 생성 성공", null));
+  }
 }
--- src/main/java/com/gamecommunity/domain/comment/service/CommentService.java
+++ src/main/java/com/gamecommunity/domain/comment/service/CommentService.java
@@ -1,5 +1,35 @@
 package com.gamecommunity.domain.comment.service;
 
+
+import com.gamecommunity.domain.comment.dto.CommentRequestDto;
+import com.gamecommunity.domain.comment.dto.CommentResponseDto;
+import com.gamecommunity.domain.comment.entity.Comment;
+import com.gamecommunity.domain.comment.repository.CommentRepository;
+import com.gamecommunity.domain.post.entity.Post;
+import com.gamecommunity.domain.post.repository.PostRepository;
+import com.gamecommunity.domain.post.service.PostService;
+import com.gamecommunity.domain.user.entity.User;
+import com.gamecommunity.global.exception.common.BusinessException;
+import com.gamecommunity.global.exception.common.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
 
+  private final CommentRepository commentRepository;
+  private final PostRepository postRepository;
+  private final PostService postService;
+
+  public CommentResponseDto createComment(User user, Long postId,
+      CommentRequestDto commentRequestDto) {
+    Post post = postRepository.findByPostId(postId).orElseThrow(() ->
+        new BusinessException(HttpStatus.BAD_REQUEST, ErrorCode.NOT_FOUND_POST_EXCEPTION));
+    Comment comment = new Comment(user, post, commentRequestDto);
+    commentRepository.save(comment);
+    return new CommentResponseDto(comment);
+  }
 }
+
