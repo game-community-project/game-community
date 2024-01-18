933407f51858fb617d3d0d056d1c72bce2637963
minseong1 <alstjd2627@naver.com>
2024-01-18 12:42:23 +0900

    feat: Team Entity, Dto 생성

--- src/main/java/com/gamecommunity/domain/team/dto/TeamRequestDto.java
+++ src/main/java/com/gamecommunity/domain/team/dto/TeamRequestDto.java
@@ -1,5 +1,17 @@
 package com.gamecommunity.domain.team.dto;
 
-public class TeamRequestDto {
+import com.gamecommunity.domain.team.entity.Team;
+import com.gamecommunity.global.enums.game.name.GameName;
+
+public record TeamRequestDto(
+    String name,
+    String introduction,
+    GameName gameName
+) {
+  public TeamRequestDto(Team team) {
+    this(team.getTeamName(),
+        team.getTeamIntroduction(),
+        team.getGameName());
+  }
 
 }
--- src/main/java/com/gamecommunity/domain/team/dto/TeamResponseDto.java
+++ src/main/java/com/gamecommunity/domain/team/dto/TeamResponseDto.java
@@ -1,5 +1,17 @@
 package com.gamecommunity.domain.team.dto;
 
-public class TeamResponseDto {
+import com.gamecommunity.domain.team.entity.Team;
+import com.gamecommunity.global.enums.game.name.GameName;
 
+public record TeamResponseDto(
+    String teamName,
+    String teamIntroduction,
+    GameName gameName
+) {
+
+  public TeamResponseDto(Team team) {
+    this(team.getTeamName(),
+        team.getTeamIntroduction(),
+        team.getGameName());
+  }
 }
