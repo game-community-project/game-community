f01d1ffb650dd2b6fac82df3e611c6ca3a88fffc
minseong1 <alstjd2627@naver.com>
2024-01-18 13:07:20 +0900

    feat : 팀 조회 기능 구현

--- src/main/java/com/gamecommunity/domain/team/controller/TeamController.java
+++ src/main/java/com/gamecommunity/domain/team/controller/TeamController.java
@@ -1,12 +1,17 @@
 package com.gamecommunity.domain.team.controller;
 
 import com.gamecommunity.domain.team.dto.TeamRequestDto;
+import com.gamecommunity.domain.team.dto.TeamResponseDto;
 import com.gamecommunity.domain.team.service.TeamService;
+import com.gamecommunity.global.enums.game.name.GameName;
 import com.gamecommunity.global.response.ApiResponse;
 import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
+import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +30,18 @@ public class TeamController {
       @RequestBody TeamRequestDto teamRequestDto) {
     teamService.createTeam(userDetails.getUser(), teamRequestDto);
     return ResponseEntity.ok(ApiResponse.ok("그룹 생성 성공", null));
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<Map<GameName, List<TeamResponseDto>>>> getTeam() {
+    Map<GameName, List<TeamResponseDto>> teamMap = teamService.getTeamsByGame();
+    return ResponseEntity.ok(ApiResponse.ok("게임 별로 속해 있는 그룹 목록 조회 성공", teamMap));
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity<ApiResponse<List<TeamResponseDto>>> getTeam(
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    List<TeamResponseDto> teamResponseDtoList = teamService.getTeamsByUser(userDetails.getUser());
+    return ResponseEntity.ok(ApiResponse.ok("유저가 속한 그룹 목록 조회 성공", teamResponseDtoList));
   }
 }
--- src/main/java/com/gamecommunity/domain/team/service/TeamService.java
+++ src/main/java/com/gamecommunity/domain/team/service/TeamService.java
@@ -1,25 +1,54 @@
 package com.gamecommunity.domain.team.service;
 
 import com.gamecommunity.domain.team.dto.TeamRequestDto;
+import com.gamecommunity.domain.team.dto.TeamResponseDto;
 import com.gamecommunity.domain.team.entity.Team;
 import com.gamecommunity.domain.team.repository.TeamRepository;
+import com.gamecommunity.domain.teamuser.repository.TeamUserRepository;
 import com.gamecommunity.domain.user.entity.User;
 import com.gamecommunity.domain.user.repository.UserRepository;
+import com.gamecommunity.global.enums.game.name.GameName;
+import com.gamecommunity.global.response.ApiResponse;
+import com.gamecommunity.global.security.userdetails.UserDetailsImpl;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 @AllArgsConstructor
 public class TeamService {
 
   private final TeamRepository teamRepository;
-  //private final TeamUserRepository teamUserRepository;
-  //private final UserRepository userRepository;
+  private final TeamUserRepository teamUserRepository;
+  private final UserRepository userRepository;
 
 
   public void createTeam(User user, TeamRequestDto teamRequestDto) {
     Team team = new Team(user.getId(), teamRequestDto);
     teamRepository.save(team);
     //addAdminUserToTeam(user,team);
+  }
+
+  public Map<GameName, List<TeamResponseDto>> getTeamsByGame() {
+    Map<GameName, List<TeamResponseDto>> teamMap = new HashMap<>();
+    for (GameName game : GameName.values()) {
+      List<TeamResponseDto> teamResponseDtos = teamRepository.findAllByGameName(game).stream()
+          .map(TeamResponseDto::new).toList();
+      teamMap.put(game, teamResponseDtos);
+    }
+    return teamMap;
+  }
+
+  public List<TeamResponseDto> getTeamsByUser(User user) {
+    List<TeamResponseDto> teamResponseDtos = teamUserRepository.findAllByUserId(user.getId())
+        .stream() // TeamUser Entity가 리스트로 나옴
+        .map(Team -> new TeamResponseDto(Team.getTeam())).toList();
+
+    return teamResponseDtos;
   }
 }
