5f4dc211e8368346b1ae5a3dee58a10de80d79e9
minseong1 <alstjd2627@naver.com>
2024-01-18 17:26:30 +0900

    feat: commentRepository

--- src/main/java/com/gamecommunity/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/gamecommunity/domain/comment/repository/CommentRepository.java
@@ -1,5 +1,16 @@
 package com.gamecommunity.domain.comment.repository;
 
-public interface CommentRepository {
 
+import com.gamecommunity.domain.comment.entity.Comment;
+import com.gamecommunity.domain.post.entity.Post;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  Page<Comment> findAllByPost(Post post, Pageable pageable);
+
+  Optional<Comment> findByCommentId(Long commentId);
 }
