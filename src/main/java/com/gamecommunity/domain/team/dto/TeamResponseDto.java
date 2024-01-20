a66f38562b8bab51e3845b52bacc8e4cf381a5f4
minseong1 <alstjd2627@naver.com>
2024-01-21 03:03:37 +0900

    refactor: team ResponseDto 변수명 변경

--- src/main/java/com/gamecommunity/domain/team/dto/TeamResponseDto.java
+++ src/main/java/com/gamecommunity/domain/team/dto/TeamResponseDto.java
@@ -2,23 +2,33 @@ package com.gamecommunity.domain.team.dto;
 
 import com.gamecommunity.domain.team.entity.Team;
 import com.gamecommunity.domain.teamuser.entity.TeamUser;
+import com.gamecommunity.domain.user.dto.UserProfileDto;
 import com.gamecommunity.global.enums.game.name.GameName;
+import com.nimbusds.openid.connect.sdk.UserInfoResponse;
+import java.util.List;
 
 public record TeamResponseDto(
+    Long teamId,
     String teamName,
     String teamIntroduction,
+    Long teamAdminId,
     GameName gameName
 ) {
 
   public TeamResponseDto(Team team) {
-    this(team.getTeamName(),
+    this(team.getTeamId(),
+        team.getTeamName(),
         team.getTeamIntroduction(),
-        team.getGameName());
+        team.getTeamAdminId(),
+        team.getGameName()
+    );
   }
 
   public TeamResponseDto(TeamUser teamUser) {
-    this(teamUser.getTeam().getTeamName(),
+    this(teamUser.getTeam().getTeamId(),
+        teamUser.getTeam().getTeamName(),
         teamUser.getTeam().getTeamIntroduction(),
+        teamUser.getTeam().getTeamAdminId(),
         teamUser.getTeam().getGameName());
   }
 }
