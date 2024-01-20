690495dac359d6bbef0edaa206e160934e43017a
Begon <japgo@naver.com>
2024-01-21 04:35:55 +0900

    fix: 주석 제거

--- src/main/java/com/gamecommunity/domain/user/service/OAuthService.java
+++ src/main/java/com/gamecommunity/domain/user/service/OAuthService.java
@@ -46,7 +46,6 @@ public class OAuthService {
 
   public TokenDto kakaoLogin(String code) throws JsonProcessingException {
     // 1. "인가 코드"로 "액세스 토큰" 요청
-//    String accessToken = (code);
     String accessToken = getToken(code);
 
     // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
