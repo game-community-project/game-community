60100a6889e78d6c7fbb785294683a9ab8f4665c
oneseel <seno10065@gmail.com>
2024-01-22 02:08:25 +0900

    fix : cors 사이트 추가

--- src/main/java/com/gamecommunity/global/config/WebConfig.java
+++ src/main/java/com/gamecommunity/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "https://game-community-project.github.io") // 허용할 출처
+        .allowedOrigins("http://localhost:3000", "https://game-community-project.github.io", "http://sparta-gameclub.s3-website.ap-northeast-2.amazonaws.com") // 허용할 출처
         .allowedHeaders("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
         .allowCredentials(true) // 쿠키 인증 요청 허용
