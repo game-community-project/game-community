62f599a587b1131ccd4aa5525dbcd78c1a0a8498
Dahee Son <sxxdxh2@gmail.com>
2024-01-25 17:50:21 +0900

    feat : 실시간 채팅 기능 초기 entity 설정

---
+++ src/main/java/com/gamecommunity/chatRoom/entity/ChatMessage.java
@@ -0,0 +1,41 @@
+package com.gamecommunity.chatRoom.entity;
+
+import com.gamecommunity.domain.user.entity.User;
+import com.gamecommunity.global.auditing.TimeStamped;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage extends TimeStamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "chatRoom_id")
+  private ChatRoom chatRoom;
+
+  private String chatContent;
+
+}
---
+++ src/main/java/com/gamecommunity/chatRoom/entity/ChatRoom.java
@@ -0,0 +1,38 @@
+package com.gamecommunity.chatRoom.entity;
+
+import com.gamecommunity.domain.post.entity.Post;
+import com.gamecommunity.global.auditing.TimeStamped;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom extends TimeStamped {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "chatRoom_id")
+  private Long id;
+
+  // 게시글과 연관된 채팅방 생성
+  // 게시글과 채팅방을 일대일로 연결
+  @OneToOne
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+  private String chatName;
+
+}
