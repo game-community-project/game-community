d0040bbde7abd818986921e452c4ae28fbd70fe3
Dahee Son <sxxdxh2@gmail.com>
2024-02-02 18:59:40 +0900

    refactor : 코드 전체적으로 리팩토링

--- src/main/java/com/gamecommunity/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/gamecommunity/chatRoom/controller/ChatRoomController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,7 +76,7 @@ public class ChatRoomController {
   }
 
   // 채팅방 나가기
-  @PostMapping("/leave/{chatRoomId}")
+  @DeleteMapping("/leave/{chatRoomId}")
   public ResponseEntity<ApiResponse> leaveChatRoom(
           @PathVariable Long chatRoomId,
           @AuthenticationPrincipal UserDetailsImpl userDetails
